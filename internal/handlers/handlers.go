db93f62325d2eeac0c707c8f8dbd7dcc3d477c5b
Oksana <oklicorne1807@gmail.com>
2026-06-21 10:36:31 +0300

    Final task 6
    Final task 6

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,54 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	result, err := service.Convert(string(data))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ext := filepath.Ext(header.Filename)
+	filename := time.Now().UTC().Format("20060102150405") + ext
+
+	err = os.WriteFile(filename, []byte(result), 0644)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(result))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(data string) (string, error) {
+	data = strings.TrimSpace(data)
+	if data == "" {
+		return "", errors.New("empty data")
+	}
+
+	if strings.ContainsAny(data, ".-") {
+		return morse.ToText(data), nil
+	}
+
+	return morse.ToMorse(data), nil
+}
